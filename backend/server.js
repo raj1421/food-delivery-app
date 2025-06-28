6ea79d48e87d24f7f21e086f3af4c3554675c2a7
Rajvardhan Patil <rajvardhanpatil8989@gmail.com>
2025-06-28 11:49:23 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import orderRouter from './routes/orderRoute.js';
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
